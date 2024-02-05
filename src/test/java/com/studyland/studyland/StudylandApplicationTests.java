36e3e443ba6d03a9760b53ec2069b0cd9553a853
radness <yhs5128@gmail.com>
2024-02-05 10:45:25 +0900

    Initial commit

---
+++ src/test/java/com/studyland/studyland/StudylandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyland.studyland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudylandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
