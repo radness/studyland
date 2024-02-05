b46dc9e60ead16180f5891b773d341d639d07c2d
radness <yhs5128@gmail.com>
2024-02-05 15:12:58 +0900

    계정 도메인

---
+++ src/main/java/com/studyland/studyland/account/AccountController.java
@@ -0,0 +1,14 @@
+package com.studyland.studyland.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/signup")
+    public String signUpForm(Model model) {
+        return "account/sign-up";
+    }
+}
---
+++ src/main/java/com/studyland/studyland/domain/Account.java
@@ -0,0 +1,56 @@
+package com.studyland.studyland.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified; // 인증된 계정인지
+
+    private String emailCheckToken; // 토큰값
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location; // 거주 지역
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdateByEmail;
+
+    private boolean studyUpdateByWeb;
+}
