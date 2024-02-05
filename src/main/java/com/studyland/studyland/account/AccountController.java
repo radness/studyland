10abab2349e442e8ba6b4d0c20cd4f12989c3a89
radness <yhs5128@gmail.com>
2024-02-05 16:26:34 +0900

    계정 도메인 수정

--- src/main/java/com/studyland/studyland/account/AccountController.java
+++ src/main/java/com/studyland/studyland/account/AccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccountController {
 
-    @GetMapping("/signup")
+    @GetMapping("/sign-up")
     public String signUpForm(Model model) {
         return "account/sign-up";
     }
---
+++ src/test/java/com/studyland/studyland/account/AccountControllerTest.java
@@ -0,0 +1,30 @@
+package com.studyland.studyland.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("회원 가입 화면이 보이는지 테스트")
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+}
