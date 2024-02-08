3adb18e438b424b45f4ba52def2e829c26ba4d56
cccenter <cccenter@miracom-inc.com>
2024-02-08 15:07:05 +0900

    회원 가입 폼 서브밋 검증

--- src/main/java/com/studyland/studyland/account/AccountController.java
+++ src/main/java/com/studyland/studyland/account/AccountController.java
@@ -1,11 +1,25 @@
 package com.studyland.studyland.account;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
@@ -15,4 +29,14 @@ public class AccountController {
         return "account/sign-up";
     }
 
+    @PostMapping("/sign-up")
+    // @ModelAttribute 는 파라미터로 쓰일 때 생략 가능.
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) { // SignUpForm에 정의한 JSR303 애너테이션 설정에 대해서 범위 제한에 걸리면 Error에 걸린다.
+            return "account/sign-up"; // error가 존재하면 다시 form으로 간다.
+        }
+
+        // TODO 회원 가입 처리
+        return "redirect:/";
+    }
 }
--- src/main/java/com/studyland/studyland/account/SignUpForm.java
+++ src/main/java/com/studyland/studyland/account/SignUpForm.java
@@ -1,12 +1,26 @@
 package com.studyland.studyland.account;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class SignUpForm {
 
+    @NotBlank // 비어 있는 값이면 안된다.
+    @Length(min = 3, max = 20)
+    // 패턴 검사 regexp : 정규식 ㄱ~ㅎ, 가~힣, a~z, 0~9, "_" , "-" 값을 지원, 값을 3개부터 20개까지의 캐릭터가 3개에서 20개까지 들어올 수 있다.
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
     private String nickname;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
+    @Length(min = 8, max = 50)
     private String password;
 
 }
