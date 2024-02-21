86277c484a041959ad955153f9203f9b03548dbb
radness <yhs5128@gmail.com>
2024-02-21 13:05:11 +0900

    스터디 도메인

---
+++ src/main/java/com/studyland/domain/Study.java
@@ -0,0 +1,43 @@
+package com.studyland.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.lang.reflect.Member;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Study {
+    @Id @GeneratedValue
+    private Long id;
+    @ManyToMany
+    private Set<Account> managers; // 관리 매니저
+    @ManyToMany
+    private Set<Member> members;
+    @Column(unique = true)
+    private String path;
+    private String title;
+    private String shortDescription;
+    // EAGER : 스터디 정보를 조회할 때 무조건 가져온다.
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String fullDescription; // 본문
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private  String image;
+    @ManyToMany
+    private Set<Tag> tags;
+    @ManyToMany
+    private Set<Tag> zones;
+    private LocalDateTime publishedDateTime;
+    private LocalDateTime closedDateTime;
+    private LocalDateTime recruitingUpdatedDateTime;
+    private boolean recruiting; // 인원 모집 중인지
+    private boolean published;
+    private boolean closed;
+    private boolean useBanner;
+
+}
+
