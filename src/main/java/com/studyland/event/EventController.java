c2f5788de23d23bc92bcc2521d76ed534555b14c
radness <yhs5128@gmail.com>
2024-02-26 09:46:30 +0900

    모임 만들기 - 생성(submit)

--- src/main/java/com/studyland/event/EventController.java
+++ src/main/java/com/studyland/event/EventController.java
@@ -2,15 +2,21 @@ package com.studyland.event;
 
 import com.studyland.account.CurrentUser;
 import com.studyland.domain.Account;
+import com.studyland.domain.Event;
 import com.studyland.domain.Study;
 import com.studyland.event.form.EventForm;
+import com.studyland.event.validator.EventValidator;
 import com.studyland.study.StudyService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.awt.*;
 
 @Controller
 @RequestMapping("/study/{path}")
@@ -18,6 +24,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EventController {
     
     private final StudyService studyService;
+    private final EventService eventService;
+    private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
+
+    @InitBinder("eventForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(eventValidator);
+    }
 
     @GetMapping("/new-event")
     public String newEventForm(@CurrentUser Account account, @PathVariable String path, Model model) {
@@ -26,5 +40,22 @@ public class EventController {
         model.addAttribute(study);
         model.addAttribute(new EventForm());
         return "event/form";
+    }
+
+    @PostMapping("/new-event")
+    public String newEventSubmit(@CurrentUser Account account, @PathVariable String path,
+                                 @Valid EventForm eventForm, Errors errors, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            model.addAttribute(study);
+            // eventForm 에 들어왔던 데이터와 binding, validation 할 때 발생하는 에러는
+            // 기본으로 model 에 담겨진다.
+            return "event/form";
+        }
+
+        // 데이터를 변경하는 작업은 서비스에 위임하겠다.
+        Event event = eventService.createEvent(modelMapper.map(eventForm, Event.class), study, account);
+        return "redirect:/study/" + study.getEncodedPath() + "/events/" + event.getId();
     }
 }
---
+++ src/main/java/com/studyland/event/EventService.java
@@ -0,0 +1,25 @@
+package com.studyland.event;
+
+import com.studyland.domain.Account;
+import com.studyland.domain.Event;
+import com.studyland.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public Event createEvent(Event event, Study study, Account account) {
+        event.setCreateBy(account);
+        event.setCreateDateTime(LocalDateTime.now());
+        event.setStudy(study);
+        return eventRepository.save(event);
+    }
+}
