c878f5d8d5d34b1db7e64fac1f24ddd08548775c
radness <yhs5128@gmail.com>
2024-03-01 16:10:38 +0900

    알람 인프라 설정

---
+++ src/main/java/com/studyland/modules/notification/study/event/StudyCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.studyland.modules.notification.study.event;
+
+import com.studyland.domain.Study;
+import lombok.Getter;
+
+@Getter
+public class StudyCreatedEvent {
+
+    private Study study;
+
+    public StudyCreatedEvent(Study study) {
+        this.study = study;
+    }
+}
